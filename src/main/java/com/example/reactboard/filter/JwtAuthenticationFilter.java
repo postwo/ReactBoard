67ba18b4fea15a5547a39e9dfbfe4c2f915e8057
postwo <codo7717@naver.com>
2024-08-13 12:48:52 +0900

    jwt provider 설정 ,filter parserVearerToken 설정

---
+++ src/main/java/com/example/reactboard/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.example.reactboard.filter;
+
+import com.example.reactboard.provider.JwtProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter { //jwt 필터
+
+    private final JwtProvider jwtProvider;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+
+
+    private String parseBearToken(HttpServletRequest request){
+
+        String authorization = request.getHeader("Authorization");
+
+        boolean hasAuthorization = StringUtils.hasText(authorization);
+        if (!hasAuthorization) return null;
+
+        boolean isBearer = authorization.startsWith("Bearer ");
+        if (!isBearer) return null;
+
+        String token = authorization.substring(7); //7번째 부터 가지고 온다
+        return token;
+    }
+}
