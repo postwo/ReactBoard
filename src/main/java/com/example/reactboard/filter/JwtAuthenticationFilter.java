9fc5a146c29299032d66819e6a186092d315d27c
postwo <codo7717@naver.com>
2024-08-20 22:45:13 +0900

    sprign security filter,config

--- src/main/java/com/example/reactboard/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/reactboard/filter/JwtAuthenticationFilter.java
@@ -6,6 +6,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -22,7 +28,37 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter { //jwt 필터
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        
+
+        try{
+            String token =  parseBearToken(request);
+
+            if (token == null){
+                filterChain.doFilter(request,response);
+                return;
+            }
+
+            String email = jwtProvider.validate(token);
+
+            if (email ==null){
+                filterChain.doFilter(request,response);
+                return;
+            }
+
+            //여기서는 email이 사용자 아이디이다 ,비밀번호 사용x , 권한
+            //AuthorityUtils.NO_AUTHORITIES 이거는 권한이 없다는 뜻이다
+            AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email,null, AuthorityUtils.NO_AUTHORITIES);
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authenticationToken);
+
+            SecurityContextHolder.setContext(securityContext);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        filterChain.doFilter(request,response); //다음 필터로 넘긴다는 뜻이다
+
 
     }
 
