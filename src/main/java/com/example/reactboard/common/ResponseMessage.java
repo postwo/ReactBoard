296ff1c08af0ad80b55880ff5f67bc397bd2c93e
postwo <codo7717@naver.com>
2024-08-21 12:55:02 +0900

    백엔드response

---
+++ src/main/java/com/example/reactboard/common/ResponseMessage.java
@@ -0,0 +1,28 @@
+package com.example.reactboard.common;
+
+public interface ResponseMessage {
+
+    //HTTP STATUS 200
+    String SUCCESS ="Success";
+
+    //HTTP STATUS 400
+    String VALIDATION_FAIL = "Validation failed"; //유효성 검증 실패
+    String DUPLICATE_EMAIL = "Duplicate email"; //중복된 이메일
+    String DUPLICATE_NICKNAME = "Duplicate nickname"; //중복된 닉네임
+    String DUPLICATE_TEL_NUMBER= "Duplicate tel number"; //중복된 전화번호
+    String NOT_EXISTED_USER = "This user does not exist"; //존재하지 않는 유저
+    String NOT_EXISTED_BOARD = "This board does not exist"; //존재하지 않는 게시물
+
+
+    //HTTP STATUS 401
+    String SIGN_IN_FAIL = "SF"; //로그인 실패
+    String AUTHORIZATION_FAIL = "AF"; //인증 실패
+
+
+    //HTTP STATUS 403
+    String NO_PERMISSION="NP"; //권한 없음
+
+
+    //HTTP STATUS 500
+    String DATABASE_ERROR = "DBE"; //데이터베이스 에러
+}
---
+++ src/main/java/com/example/reactboard/dto/response/ResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.reactboard.dto.response;
+
+import com.example.reactboard.common.ResponseCode;
+import com.example.reactboard.common.ResponseMessage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String code;
+    private String message;
+
+    public static ResponseEntity<ResponseDto> databaseError(){
+        ResponseDto responseBody = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+
+
+}
